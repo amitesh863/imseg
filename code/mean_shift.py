a0c6297424210bd42fb657cd92ce9a771557013c
Durgesh Singh <durgesh93@outlook.com>
2018-10-28 16:42:07 +0530

    Mean shift code submitted for testing phase

--- code/mean_shift.py
+++ code/mean_shift.py
@@ -8,10 +8,12 @@ Created on Fri Oct 26 18:52:37 2018
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
 
 
 input_dir = '../data/'
 output_dir = '../result/'
+
 
 
 
@@ -35,6 +37,27 @@ def calc_density_mean(neig,seed,rad):
     mean = np.array(np.sum(weights[:,None]*neig,axis=0)/np.sum(weights),dtype=np.int64)
     return mean
     
+def coalesce_means(means,bandwidth):
+    flags = [1 for me in means]
+    for i in range(len(means)):
+        if flags[i] == 1:
+            w = 1.0
+            j = i + 1
+            while j < len(means):
+                dsm = np.linalg.norm(means[i] - means[j])
+                if dsm < bandwidth:
+                    means[i] = means[i] + means[j]
+                    w = w + 1.0
+                    flags[j] = 0
+                j = j + 1
+            means[i] = means[i]/w
+    converged_means = []
+    for i in range(len(means)):
+        if flags[i] == 1:
+            converged_means.append(means[i])
+    converged_means = np.array(converged_means)
+    return converged_means
+
 
 #function of kernel density estimation
 def kde(data_arr,mean_indx,rad):
@@ -59,13 +82,14 @@ def kde(data_arr,mean_indx,rad):
 
 #reading the image in RGB format
 
-arr_img= np.array(Image.open(input_dir+'cameraman.tif').resize((256,256)))
+arr_img= np.array(Image.open(input_dir+'input2.jpg').resize((256,256)))
+
 '''
 plt.figure()
 plt.imshow(arr_img)
 plt.show()
 '''
-
+rad = 7
 #handling grayscale vs color case
 if (len(arr_img.shape) == 2):
     w,h = arr_img.shape
@@ -73,15 +97,17 @@ if (len(arr_img.shape) == 2):
 elif(len(arr_img.shape) == 3):
     w,h,d = arr_img.shape
     data_arr = arr_img.reshape((w*h,d))    
+no_probes = 50
 
 
 #selecting random means given by k
-mean_indx = np.random.choice(data_arr.shape[0],3,replace=True)
+mean_indx = np.random.choice(data_arr.shape[0],no_probes,replace=True)
 #performing kde for each mean
-final_means = kde(data_arr,mean_indx,3)
+kde_means = kde(data_arr,mean_indx,rad)
 #assigning the clusters
+clus_data_arr = assign_cluster(data_arr,kde_means)
+final_means = coalesce_means(kde_means,rad)
 clus_data_arr = assign_cluster(data_arr,final_means)
-
 
 
 #for each mean and its corresponding cluster
